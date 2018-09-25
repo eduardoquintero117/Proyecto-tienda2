d2a30e2aaa24dd90a3885dbeab07fc2d9c08756f
Eduardo Quintero <eduardoquintero@macbook-air-de-eduardo.local>
2018-09-25 13:57:21 -0500

    EJEMPLO

--- Proyecto tienda/ViewController.swift
+++ Proyecto tienda/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//experimento
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("HOLA MUNDO")
+        
     }
 
     override func didReceiveMemoryWarning() {
